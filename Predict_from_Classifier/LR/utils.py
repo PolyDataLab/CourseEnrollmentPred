a6ff08e51dcdc2cf0f4040daeb44bbc50571c4dd
Md Akib Zabed Khan <38156511+akibzabedkhan@users.noreply.github.com>
2024-04-06 17:46:22 -0400

    Add files via upload

--- Predict_from_Classifier/LR/utils.py
+++ Predict_from_Classifier/LR/utils.py
@@ -1,26 +1,5 @@
 import os
-#calculating offered courses in each semster and storing the list of courses in a dictionary
-# def calculate_offered_dict(offered_course_dict, index2, basket):
-#     if index2 in offered_course_dict:
-#         list_items= offered_course_dict[index2] #list of courses at a semester
-#     else:
-#         list_items = []
-#     for item2 in basket:
-#         if item2 not in list_items:
-#             list_items.append(item2)
-#     offered_course_dict[index2] = list_items.copy()
-#     return offered_course_dict
 
-# def calculate_offered_courses(data3):
-#     offered_course_dict = {}
-#     index1= 1
-#     for baskets in data3['baskets']:
-#         index1= 1
-#         for basket in baskets:
-#             #offered courses at each semester
-#             offered_course_dict = calculate_offered_dict(offered_course_dict, index1, basket)
-#             index1+=1
-#     return offered_course_dict
 
 #if the course is available in the user's previous baskets or course is not available in the offered list for that semester, we filer out this course
 def filtering (item3, user_baskets, offered_course_list, item_dict):
